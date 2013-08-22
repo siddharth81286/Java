9f1e124b07dffda244f634347ffa3217a734c3f6
Syed Sayem <syed@hotmail.com>
2013-08-21 22:47:29 -0400

    Added Java code Example2.java

--- src/main/java/com/sayem/Example2.java
+++ src/main/java/com/sayem/Example2.java
@@ -1,11 +1,17 @@
 package com.sayem;
 
-/**
- * Created with IntelliJ IDEA.
- * User: sayem
- * Date: 8/21/13
- * Time: 10:46 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Example2 {
+class Example2 {
+    public static void main(String args[]) {
+        int var1; // this declares a variable
+        int var2; // this declares another variable
+
+        var1 = 1024; // this assigns 1024 to var1
+
+        System.out.println("var1 contains " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.print("var2 contains var1 / 2: ");
+        System.out.println(var2);
+    }
 }
