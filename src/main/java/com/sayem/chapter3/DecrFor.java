89af6c91bcaf25e1295ccb2aff899e3185ac190a
Syed Sayem <syed@hotmail.com>
2013-08-23 08:37:33 -0400

    Added Java code DecrFor.java

---
+++ src/main/java/com/sayem/chapter3/DecrFor.java
@@ -0,0 +1,10 @@
+package com.sayem.chapter3;
+
+class DecrFor {
+    public static void main(String args[]) {
+        int x;
+
+        for(x = 100; x > -100; x -= 5)
+            System.out.println(x);
+    }
+}
