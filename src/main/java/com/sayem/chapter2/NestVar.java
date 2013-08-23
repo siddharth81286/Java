0f9f16bd9ad2b7434d00e0ba3e7fc452ca3459c2
Syed Sayem <syed@hotmail.com>
2013-08-22 20:54:36 -0400

    Added Java code NestVar.java

---
+++ src/main/java/com/sayem/chapter2/NestVar.java
@@ -0,0 +1,15 @@
+package com.sayem.chapter2;
+
+class NestVar {
+    public static void main(String args[]) {
+        int count;
+
+        for(count = 0; count < 10; count = count+1) {
+            System.out.println("This is count: " + count);
+
+           // int count; // illegal!!!
+            for(count = 0; count < 2; count++)
+                System.out.println("This program is in error!");
+        }
+    }
+}
