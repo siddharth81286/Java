cb3994b2fa58052bedad357f2e75cf88d4430cf6
Syed Sayem <syed@hotmail.com>
2013-08-25 19:23:10 -0400

    Added Vehicle0 class

---
+++ src/main/java/com/sayem/chapter4/Vehicle0.java
@@ -0,0 +1,17 @@
+package com.sayem.chapter4;
+
+class Vehicle0 {
+    int passengers; // number of passengers
+    int fuelcap;    // fuel capacity in gallons
+    int mpg;        // fuel consumption in miles per gallon
+
+    // Return the range.
+    int range() {
+        return mpg * fuelcap;
+    }
+
+    // Compute fuel needed for a given distance.
+    double fuelneeded(int miles) {
+        return (double) miles / mpg;
+    }
+}
