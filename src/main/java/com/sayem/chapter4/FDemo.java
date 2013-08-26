8d37813640f7a3a23826e8611af85dd3f53e6113
Syed Sayem <syed@hotmail.com>
2013-08-26 08:51:07 -0400

    Added FDemo class

---
+++ src/main/java/com/sayem/chapter4/FDemo.java
@@ -0,0 +1,20 @@
+package com.sayem.chapter4;
+
+class FDemo {
+    int x;
+
+    FDemo(int i) {
+        x = i;
+    }
+
+    // called when object is recycled
+    protected void finalize() {
+        System.out.println("Finalizing " + x);
+    }
+
+    // generates an object that is immediately destroyed
+    void generator(int i) {
+        FDemo o = new FDemo(i);
+    }
+
+}
