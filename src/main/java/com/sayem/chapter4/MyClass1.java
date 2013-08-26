9f6b0a44cf343b1bf1a91b2b7ddca96c017261fa
Syed Sayem <syed@hotmail.com>
2013-08-26 08:49:50 -0400

    Modified MyClass1

--- src/main/java/com/sayem/chapter4/MyClass1.java
+++ src/main/java/com/sayem/chapter4/MyClass1.java
@@ -10,8 +10,8 @@ class MyClass1 {
 
 class ParmConsDemo {
     public static void main(String args[]) {
-        MyClass t1 = new MyClass(10);
-        MyClass t2 = new MyClass(88);
+        MyClass1 t1 = new MyClass1(10);
+        MyClass1 t2 = new MyClass1(88);
 
         System.out.println(t1.x + " " + t2.x);
     }
