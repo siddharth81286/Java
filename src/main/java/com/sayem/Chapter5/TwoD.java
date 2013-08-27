31f6c409c4c1ee3d1018ba097507ce527121d165
Syed Sayem <syed@hotmail.com>
2013-08-27 08:23:56 -0400

    Added TwoD class

---
+++ src/main/java/com/sayem/Chapter5/TwoD.java
@@ -0,0 +1,16 @@
+package com.sayem.Chapter5;
+
+class TwoD {
+    public static void main(String args[]) {
+        int t, i;
+        int table[][] = new int[3][4];
+
+        for(t=0; t < 3; ++t) {
+            for(i=0; i < 4; ++i) {
+                table[t][i] = (t*4)+i+1;
+                System.out.print(table[t][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
