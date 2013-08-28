0904b271642ec01342121338c270175f78b15cfc
Syed Sayem <syed@hotmail.com>
2013-08-28 08:19:36 -0400

    Added Queue class

---
+++ src/main/java/com/sayem/Chapter5/Queue.java
@@ -0,0 +1,33 @@
+package com.sayem.Chapter5;
+
+class Queue {
+    char q[]; // this array holds the queue
+    int putloc, getloc; // the put and get indices
+
+    Queue(int size) {
+        q = new char[size+1]; // allocate memory for queue
+        putloc = getloc = 0;
+    }
+
+    // put a characer into the queue
+    void put(char ch) {
+        if(putloc==q.length-1) {
+            System.out.println(" -- Queue is full.");
+            return;
+        }
+
+        putloc++;
+        q[putloc] = ch;
+    }
+
+    // get a character from the queue
+    char get() {
+        if(getloc == putloc) {
+            System.out.println(" -- Queue is empty.");
+            return (char) 0;
+        }
+
+        getloc++;
+        return q[getloc];
+    }
+}
