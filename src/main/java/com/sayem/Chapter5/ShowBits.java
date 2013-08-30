a77acedae820729b2dabb67b4ce01e182dd0a578
Syed Sayem <syed@hotmail.com>
2013-08-30 08:21:28 -0400

    Added ShowBits class

--- src/main/java/com/sayem/Chapter5/ShowBits.java
+++ src/main/java/com/sayem/Chapter5/ShowBits.java
@@ -1,5 +1,6 @@
 package com.sayem.Chapter5;
 
+/*
 class ShowBits {
     public static void main(String args[]) {
         int t;
@@ -10,5 +11,34 @@ class ShowBits {
             if((val & t) != 0) System.out.print("1 ");
             else System.out.print("0 ");
         }
+    }
+}
+
+*/
+
+class ShowBits {
+    int numbits;
+
+    ShowBits(int n) {
+        numbits = n;
+    }
+
+    void show(long val) {
+        long mask = 1;
+
+        // left-shit a 1 into the proper position
+        mask <<= numbits-1;
+
+        int spacer = 0;
+        for(; mask != 0; mask >>>= 1) {
+            if((val & mask) != 0) System.out.print("1");
+            else System.out.print("0");
+            spacer++;
+            if((spacer % 8) == 0) {
+                System.out.print(" ");
+                spacer = 0;
+            }
+        }
+        System.out.println();
     }
 }
