e70fa0184555c54aebb1364974d8d4bc783663b5
Syed Sayem <syed@hotmail.com>
2013-08-30 08:22:24 -0400

    Added ShowBitsDemo class

---
+++ src/main/java/com/sayem/Chapter5/ShowBitsDemo.java
@@ -0,0 +1,23 @@
+package com.sayem.Chapter5;
+
+class ShowBitsDemo {
+    public static void main(String args[]) {
+        ShowBits b = new ShowBits(8);
+        ShowBits i = new ShowBits(32);
+        ShowBits li = new ShowBits(64);
+
+        System.out.println("123 in binary: ");
+        b.show(123);
+
+        System.out.println("\n87987 in binary: ");
+        i.show(87987);
+
+        System.out.println("\n237658768 in binary: ");
+        li.show(237658768);
+
+
+        // you can also show low-order bits of any integer
+        System.out.println("\nLow order 8 bits of 87987 in binary: ");
+        b.show(87987);
+    }
+}
