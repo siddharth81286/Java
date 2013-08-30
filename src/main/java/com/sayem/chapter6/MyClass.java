0782dc723fec9d2bb20433db2c71fc084202323b
Syed Sayem <syed@hotmail.com>
2013-08-30 08:25:43 -0400

    Added MyClass class

---
+++ src/main/java/com/sayem/chapter6/MyClass.java
@@ -0,0 +1,19 @@
+package com.sayem.chapter6;
+
+class MyClass {
+    private int alpha; // private access
+    public int beta; // public access
+    int gamma; // default access (essentially public)
+
+    /* Methods to access alpha.  It is OK for a
+       member of a class to access a private member
+       of the same class.
+    */
+    void setAlpha(int a) {
+        alpha = a;
+    }
+
+    int getAlpha() {
+        return alpha;
+    }
+}
