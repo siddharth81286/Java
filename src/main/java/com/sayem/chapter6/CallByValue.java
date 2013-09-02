9bb26b3a666ebb967a16faeb64137117eb06a226
Syed Sayem <syed@hotmail.com>
2013-09-02 08:45:53 -0400

    Added CallByValue class

---
+++ src/main/java/com/sayem/chapter6/CallByValue.java
@@ -0,0 +1,17 @@
+package com.sayem.chapter6;
+
+class CallByValue {
+    public static void main(String args[]) {
+        Test ob = new Test();
+
+        int a = 15, b = 20;
+
+        System.out.println("a and b before call: " +
+                a + " " + b);
+
+        ob.noChange(a, b);
+
+        System.out.println("a and b after call: " +
+                a + " " + b);
+    }
+}
