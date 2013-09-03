5fb6e3a0d4a665457af717778e28666f8126adcc
Syed Sayem <syed@hotmail.com>
2013-09-03 08:22:48 -0400

    Modified MyClass class

--- src/main/java/com/sayem/chapter6/MyClass.java
+++ src/main/java/com/sayem/chapter6/MyClass.java
@@ -1,19 +1,25 @@
 package com.sayem.chapter6;
 
 class MyClass {
-    private int alpha; // private access
-    public int beta; // public access
-    int gamma; // default access (essentially public)
+    int x;
 
-    /* Methods to access alpha.  It is OK for a
-       member of a class to access a private member
-       of the same class.
-    */
-    void setAlpha(int a) {
-        alpha = a;
+    MyClass() {
+        System.out.println("Inside MyClass().");
+        x = 0;
     }
 
-    int getAlpha() {
-        return alpha;
+    MyClass(int i) {
+        System.out.println("Inside MyClass(int).");
+        x = i;
+    }
+
+    MyClass(double d) {
+        System.out.println("Inside MyClass(double).");
+        x = (int) d;
+    }
+
+    MyClass(int i, int j) {
+        System.out.println("Inside MyClass(int, int).");
+        x = i * j;
     }
 }
