bb094d4fca1cbccc026c1a4524c8af7e5b9c5be5
Syed Sayem <syed@hotmail.com>
2013-09-03 08:18:04 -0400

    Added Err class

---
+++ src/main/java/com/sayem/chapter6/Err.java
@@ -0,0 +1,11 @@
+package com.sayem.chapter6;
+
+class Err {
+    String msg; // error message
+    int severity; // code indicating severity of error
+
+    Err(String m, int s) {
+        msg = m;
+        severity  = s;
+    }
+}
