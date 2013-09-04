952ee5ee17c1c84f408090d9465ea4f88242930a
Syed Sayem <syed@hotmail.com>
2013-09-04 08:20:49 -0400

    Modified Queue class

--- src/main/java/com/sayem/chapter6/Queue.java
+++ src/main/java/com/sayem/chapter6/Queue.java
@@ -1,13 +1,33 @@
 package com.sayem.chapter6;
 
 class Queue {
-    // these members are now private
     private char q[]; // this array holds the queue
     private int putloc, getloc; // the put and get indices
 
+    // Construct an empty Queue given its size.
     Queue(int size) {
         q = new char[size+1]; // allocate memory for queue
         putloc = getloc = 0;
+    }
+
+    // Construct a Queue from a Queue.
+    Queue(Queue ob) {
+        putloc = ob.putloc;
+        getloc = ob.getloc;
+        q = new char[ob.q.length];
+
+        // copy elements
+        for(int i=getloc+1; i <= putloc; i++)
+            q[i] = ob.q[i];
+    }
+
+    // Construct a Queue with initial values.
+    Queue(char a[]) {
+        putloc = 0;
+        getloc = 0;
+        q = new char[a.length+1];
+
+        for(int i = 0; i < a.length; i++) put(a[i]);
     }
 
     // Put a characer into the queue.
