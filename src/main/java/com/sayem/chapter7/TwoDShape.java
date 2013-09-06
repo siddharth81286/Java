09432aab9ee7175188bf17d4a68432d130ac3b78
Syed Sayem <syed@hotmail.com>
2013-09-06 08:20:29 -0400

    Modified TwoDShape class

--- src/main/java/com/sayem/chapter7/TwoDShape.java
+++ src/main/java/com/sayem/chapter7/TwoDShape.java
@@ -4,6 +4,12 @@ class TwoDShape {
     private double width;  // these are
     private double height; // now private
 
+    // Accessor methods for width and height.
+    double getWidth() { return width; }
+    double getHeight() { return height; }
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+
     void showDim() {
         System.out.println("Width and height are " +
                 width + " and " + height);
