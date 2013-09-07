90e68c8486150ab75cf32a487f917927506c5931
Syed Sayem <syed@hotmail.com>
2013-09-07 11:26:15 -0400

    Added bunch of different stuff

--- src/main/java/com/sayem/chapter7/Triangle.java
+++ src/main/java/com/sayem/chapter7/Triangle.java
@@ -3,10 +3,24 @@ package com.sayem.chapter7;
 class Triangle extends TwoDShape {
     private String style;
 
+    // A default constructor.
+    Triangle() {
+        super();
+        style = "none";
+    }
+
+    // Constructor
     Triangle(String s, double w, double h) {
         super(w, h); // call superclass constructor
 
         style = s;
+    }
+
+    // One argument constructor.
+    Triangle(double x) {
+        super(x); // call superclass constructor
+
+        style = "filled";
     }
 
     double area() {
