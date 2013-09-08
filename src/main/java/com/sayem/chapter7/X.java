23d19f09b9c0c280ee34840c361eb9b3466a04c9
Syed Sayem <syed@hotmail.com>
2013-09-08 11:17:45 -0400

    Fixed error in X class

--- src/main/java/com/sayem/chapter7/X.java
+++ src/main/java/com/sayem/chapter7/X.java
@@ -19,7 +19,7 @@ class IncompatibleRef {
     public static void main(String args[]) {
         X x = new X(10);
         X x2;
-        Y y = new Y(5);
+        //Y y = new Y(5);
 
         x2 = x; // OK, both of same type
 
