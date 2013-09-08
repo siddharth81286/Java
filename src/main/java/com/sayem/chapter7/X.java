31abb6b4141bb6acb6114848d002be85a7a3eb47
Syed Sayem <syed@hotmail.com>
2013-09-08 11:17:22 -0400

    Modified Class x

--- src/main/java/com/sayem/chapter7/X.java
+++ src/main/java/com/sayem/chapter7/X.java
@@ -6,10 +6,13 @@ class X {
     X(int i) { a = i; }
 }
 
-class Y {
-    int a;
+class Y extends X {
+    int b;
 
-    Y(int i) { a = i; }
+    Y(int i, int j) {
+        super(j);
+        b = i;
+    }
 }
 
 class IncompatibleRef {
