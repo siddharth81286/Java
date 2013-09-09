ee64a400b43d3d55bf8de9b6fcbc285f3bab46f1
Syed Sayem <syed@hotmail.com>
2013-09-09 08:31:00 -0400

    Added Book class

---
+++ src/main/java/com/sayem/chapter8/bookpack/bookpack.java
@@ -0,0 +1,21 @@
+package com.sayem.chapter8.bookpack;
+
+class Book {
+    private String title;
+    private String author;
+    private int pubDate;
+
+    Book(String t, String a, int d) {
+        title = t;
+        author = a;
+        pubDate = d;
+    }
+
+    void show() {
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(pubDate);
+        System.out.println();
+    }
+}
+
