19ad8394b161203b3511d5fb0865bf4b71283a9e
Syed Sayem <syed@hotmail.com>
2013-09-09 08:32:02 -0400

    Added BookDemo class

---
+++ src/main/java/com/sayem/chapter8/bookpack/BookDemo.java
@@ -0,0 +1,11 @@
+package com.sayem.chapter8.bookpack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sayem
+ * Date: 9/9/13
+ * Time: 8:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookDemo {
+}
