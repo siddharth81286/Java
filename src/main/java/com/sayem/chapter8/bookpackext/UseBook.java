2f8df5cf4d53af8c3a2eb20e340e93960ce0a2bc
Syed Sayem <syed@hotmail.com>
2013-09-10 08:36:38 -0400

    Added Series interface

--- src/main/java/com/sayem/chapter8/bookpackext/UseBook.java
+++ src/main/java/com/sayem/chapter8/bookpackext/UseBook.java
@@ -3,17 +3,17 @@ package com.sayem.chapter8.bookpackext;
 // Use the Book Class from bookpack.
 class UseBook {
     public static void main(String args[]) {
-        com.sayem.chapter8.bookpack.Book books[] = new com.sayem.chapter8.bookpack.Book[5];
+        Book books[] = new Book[5];
 
-        books[0] = new com.sayem.chapter8.bookpack.Book("Java: A Beginner's Guide",
+        books[0] = new Book("Java: A Beginner's Guide",
                 "Schildt", 2011);
-        books[1] = new com.sayem.chapter8.bookpack.Book("Java: The Complete Reference",
+        books[1] = new Book("Java: The Complete Reference",
                 "Schildt", 2011);
-        books[2] = new com.sayem.chapter8.bookpack.Book("The Art of Java",
+        books[2] = new Book("The Art of Java",
                 "Schildt and Holmes", 2003);
-        books[3] = new com.sayem.chapter8.bookpack.Book("Red Storm Rising",
+        books[3] = new Book("Red Storm Rising",
                 "Clancy", 1986);
-        books[4] = new com.sayem.chapter8.bookpack.Book("On the Road",
+        books[4] = new Book("On the Road",
                 "Kerouac", 1955);
 
         for(int i=0; i < books.length; i++) books[i].show();
