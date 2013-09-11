6df0b3b75fed0a69b5ff22f48ad913cfbb8aafde
Syed Sayem <syed@hotmail.com>
2013-09-11 08:34:01 -0400

    Added IFExtend class

---
+++ src/main/java/com/sayem/chapter8/IFExtend.java
@@ -0,0 +1,11 @@
+package com.sayem.chapter8;
+
+class IFExtend {
+    public static void main(String arg[]) {
+        MyClass ob = new MyClass();
+
+        ob.meth1();
+        ob.meth2();
+        ob.meth3();
+    }
+}
