07ca3d60c6eba5037ab0978b526aa09c66ab1cc7
Syed Sayem <syed@hotmail.com>
2013-09-13 08:14:42 -0400

    Added RethrowDemo

---
+++ src/main/java/com/sayem/chapter9/RethrowDemo.java
@@ -0,0 +1,14 @@
+package com.sayem.chapter9;
+
+class RethrowDemo {
+    public static void main(String args[]) {
+        try {
+            Rethrow.genException();
+        }
+        catch(ArrayIndexOutOfBoundsException exc) {
+            // recatch exception
+            System.out.println("Fatal error -- " +
+                    "program terminated.");
+        }
+    }
+}
