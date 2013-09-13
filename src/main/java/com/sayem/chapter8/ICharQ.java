00f6016dc0437586ea9935566100b4c19385dea7
Syed Sayem <syed@hotmail.com>
2013-09-13 08:26:32 -0400

    Added FixedQueue class

--- src/main/java/com/sayem/chapter8/ICharQ.java
+++ src/main/java/com/sayem/chapter8/ICharQ.java
@@ -1,9 +1,12 @@
 package com.sayem.chapter8;
 
+import com.sayem.chapter9.QueueEmptyException;
+import com.sayem.chapter9.QueueFullException;
+
 public interface ICharQ {
     // Put a characer into the queue.
-    void put(char ch);
+    void put(char ch) throws QueueFullException;
 
     // Get a character from the queue.
-    char get();
+    char get() throws QueueEmptyException;
 }
