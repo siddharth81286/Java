ea7b6be9fbb86bbfe0b5d0bafc66995b4142baf7
Syed Sayem <syed@hotmail.com>
2013-09-14 10:36:04 -0400

    Modified CopyFile class

--- src/main/java/com/sayem/chapter10/CopyFile.java
+++ src/main/java/com/sayem/chapter10/CopyFile.java
@@ -1,13 +1,9 @@
 package com.sayem.chapter10;
+/* A version of CopyFile that uses try-with-resources.
+   It demonstrates two resources (in this case files) being
+   managed by a single try statement.
 
-/* Copy a text file.
-   To use this program, specify the name
-   of the source file and the destination file.
-   For example, to copy a file called FIRST.TXT
-   to a file called SECOND.TXT, use the following
-   command line.
-
-   java CopyFile FIRST.TXT SECOND.TXT
+   Note: This code requires JDK 7 or later.
 */
 
 import java.io.*;
@@ -16,20 +12,17 @@ class CopyFile {
     public static void main(String args[]) throws IOException
     {
         int i;
-        FileInputStream fin = null;
-        FileOutputStream fout = null;
 
-        // First, make sure that both files has been specified.
+        // First, confirm that both files has been specified.
         if(args.length != 2) {
             System.out.println("Usage: CopyFile from to");
             return;
         }
 
-        // Copy a File.
-        try {
-            // Attempt to open the files.
-            fin = new FileInputStream(args[0]);
-            fout = new FileOutputStream(args[1]);
+        // Open and manage two files via the try statement.
+        try (FileInputStream fin = new FileInputStream(args[0]);
+             FileOutputStream fout = new FileOutputStream(args[1]))
+        {
 
             do {
                 i = fin.read();
@@ -38,17 +31,6 @@ class CopyFile {
 
         } catch(IOException exc) {
             System.out.println("I/O Error: " + exc);
-        } finally {
-            try {
-                if(fin != null) fin.close();
-            } catch(IOException exc) {
-                System.out.println("Error Closing Input File");
-            }
-            try {
-                if(fout != null) fout.close();
-            } catch(IOException exc) {
-                System.out.println("Error Closing Output File");
-            }
         }
     }
 }
