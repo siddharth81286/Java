c0c11a3cd064e052994cce0d6e848c358b77f5b0
Syed Sayem <syed@hotmail.com>
2013-09-14 10:33:54 -0400

    Modified ShowFile class

--- src/main/java/com/sayem/chapter10/ShowFile.java
+++ src/main/java/com/sayem/chapter10/ShowFile.java
@@ -1,13 +1,8 @@
 package com.sayem.chapter10;
 
-/* Display a text file.
-
-   To use this program, specify the name
-   of the file that you want to see.
-   For example, to see a file called TEST.TXT,
-   use the following command line.
-
-   java ShowFile TEST.TXT
+/* This variation wraps the code that opens and
+   accesses the file within a single try block.
+   The file is closed by the finally block.
 */
 
 import java.io.*;
@@ -16,36 +11,35 @@ class ShowFile {
     public static void main(String args[])
     {
         int i;
-        FileInputStream fin;
+        FileInputStream fin = null;
 
-        // First make sure that a file has been specified.
+        // First, confirm that a file name has been specified.
         if(args.length != 1) {
-            System.out.println("Usage: ShowFile File");
+            System.out.println("Usage: ShowFile filename");
             return;
         }
 
+        // The following code opens a file, reads characters until EOF
+        // is encountered, and then closes the file via a finally block.
         try {
             fin = new FileInputStream(args[0]);
-        } catch(FileNotFoundException exc) {
-            System.out.println("File Not Found");
-            return;
-        }
 
-        try {
-            // read bytes until EOF is encountered
             do {
                 i = fin.read();
                 if(i != -1) System.out.print((char) i);
             } while(i != -1);
-        } catch(IOException exc) {
-            System.out.println("Error reading file.");
-        }
 
-        try {
-            fin.close();
+        } catch(FileNotFoundException exc) {
+            System.out.println("File Not Found.");
         } catch(IOException exc) {
-            System.out.println("Error closing file.");
+            System.out.println("An I/O Error Occurred");
+        } finally {
+            // Close file in all cases.
+            try {
+                if(fin != null) fin.close();
+            } catch(IOException exc) {
+                System.out.println("Error Closing File");
+            }
         }
-
     }
 }
