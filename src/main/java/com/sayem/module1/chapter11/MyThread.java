ce18d07170ae63700435e09774aa4810c6f286e1
Syed Sayem <syed@hotmail.com>
2013-09-16 08:24:23 -0400

    Improved MyThreads with multiple threads

--- src/main/java/com/sayem/module1/chapter11/MyThread.java
+++ src/main/java/com/sayem/module1/chapter11/MyThread.java
@@ -1,32 +1,30 @@
 package com.sayem.module1.chapter11;
 
-/*
-   Try This 11-1
+// Create multiple threads.
 
-   Extend Thread.
-*/
-class MyThread extends Thread {
+class MyThread implements Runnable {
+    Thread thrd;
 
     // Construct a new thread.
     MyThread(String name) {
-        super(name); // name thread
-        start(); // start the thread
+        thrd = new Thread(this, name);
+
+        thrd.start(); // start the thread
     }
 
     // Begin execution of new thread.
     public void run() {
-        System.out.println(getName() + " starting.");
+        System.out.println(thrd.getName() + " starting.");
         try {
             for(int count=0; count < 10; count++) {
                 Thread.sleep(400);
-                System.out.println("In " + getName() +
+                System.out.println("In " + thrd.getName() +
                         ", count is " + count);
             }
         }
         catch(InterruptedException exc) {
-            System.out.println(getName() + " interrupted.");
+            System.out.println(thrd.getName() + " interrupted.");
         }
-
-        System.out.println(getName() + " terminating.");
+        System.out.println(thrd.getName() + " terminating.");
     }
 }
