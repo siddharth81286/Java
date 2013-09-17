6fc0937cf43d4ae772c3ebbf64e835b2dacab2a7
Syed Sayem <syed@hotmail.com>
2013-09-17 08:27:47 -0400

    Improved MyThread class

--- src/main/java/com/sayem/module1/chapter11/MyThread.java
+++ src/main/java/com/sayem/module1/chapter11/MyThread.java
@@ -1,29 +1,28 @@
 package com.sayem.module1.chapter11;
 
-// Use join().
-
 class MyThread implements Runnable {
     Thread thrd;
+    static SumArray sa = new SumArray();
+    int a[];
+    int answer;
 
     // Construct a new thread.
-    MyThread(String name) {
+    MyThread(String name, int nums[]) {
         thrd = new Thread(this, name);
+        a = nums;
         thrd.start(); // start the thread
     }
 
     // Begin execution of new thread.
     public void run() {
+        int sum;
+
         System.out.println(thrd.getName() + " starting.");
-        try {
-            for(int count=0; count < 10; count++) {
-                Thread.sleep(400);
-                System.out.println("In " + thrd.getName() +
-                        ", count is " + count);
-            }
-        }
-        catch(InterruptedException exc) {
-            System.out.println(thrd.getName() + " interrupted.");
-        }
+
+        answer = sa.sumArray(a);
+        System.out.println("Sum for " + thrd.getName() +
+                " is " + answer);
+
         System.out.println(thrd.getName() + " terminating.");
     }
 }
