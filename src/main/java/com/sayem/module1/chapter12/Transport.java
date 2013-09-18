c43d717de0a3849cb7944ee6a29da6bfb76ba17e
Syed Sayem <syed@hotmail.com>
2013-09-18 08:24:32 -0400

    Improved Transport enum class

--- src/main/java/com/sayem/module1/chapter12/Transport.java
+++ src/main/java/com/sayem/module1/chapter12/Transport.java
@@ -1,6 +1,13 @@
 package com.sayem.module1.chapter12;
 
-// An enumeration of Transport varieties.
+// Use an enum constructor, instance variable, and method.
 enum Transport {
-    CAR, TRUCK, AIRPLANE, TRAIN, BOAT
+    CAR(65), TRUCK(55), AIRPLANE(600), TRAIN(70), BOAT(22);
+
+    private int speed; // typical speed of each transport
+
+    // Constructor
+    Transport(int s) { speed = s; }
+
+    int getSpeed() { return speed; }
 }
