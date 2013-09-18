96999174adb4e3e949fb04f237a77bdafcf6d905
Syed Sayem <syed@hotmail.com>
2013-09-18 08:26:53 -0400

    Added EnumDemo4 class

---
+++ src/main/java/com/sayem/module1/chapter12/listing4/Transport.java
@@ -0,0 +1,8 @@
+package com.sayem.module1.chapter12.listing4;
+
+// Demonstrate ordinal() and compareTo().
+
+// An enumeration of Transport varieties.
+enum Transport {
+    CAR, TRUCK, AIRPLANE, TRAIN, BOAT
+}
