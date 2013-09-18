9de8edeafadfe1f4fcd795dec03cf971f7bbee5e
Syed Sayem <syed@hotmail.com>
2013-09-18 08:18:33 -0400

    Improved TickTock class

--- src/main/java/com/sayem/module1/chapter11/TickTock.java
+++ src/main/java/com/sayem/module1/chapter11/TickTock.java
@@ -1,7 +1,6 @@
 package com.sayem.module1.chapter11;
 
-// Use wait() and notify() to create a ticking clock.
-
+// No calls to wait() or notify().
 class TickTock {
 
     String state; // contains the state of the clock
@@ -9,42 +8,22 @@ class TickTock {
     synchronized void tick(boolean running) {
         if(!running) { // stop the clock
             state = "ticked";
-            notify(); // notify any waiting threads
             return;
         }
 
         System.out.print("Tick ");
 
         state = "ticked"; // set the current state to ticked
-
-        notify(); // let tock() run
-        try {
-            while(!state.equals("tocked"))
-                wait(); // wait for tock() to complete
-        }
-        catch(InterruptedException exc) {
-            System.out.println("Thread interrupted.");
-        }
     }
 
     synchronized void tock(boolean running) {
         if(!running) { // stop the clock
             state = "tocked";
-            notify(); // notify any waiting threads
             return;
         }
 
         System.out.println("Tock");
 
         state = "tocked"; // set the current state to tocked
-
-        notify(); // let tick() run
-        try {
-            while(!state.equals("ticked"))
-                wait(); // wait for tick to complete
-        }
-        catch(InterruptedException exc) {
-            System.out.println("Thread interrupted.");
-        }
     }
 }
