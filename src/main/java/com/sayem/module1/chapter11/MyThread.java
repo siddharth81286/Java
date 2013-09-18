3eb4c0633c0ff639fc9867f3a9e0d91d7bdd91a1
Syed Sayem <syed@hotmail.com>
2013-09-18 08:20:07 -0400

    Updated MyThread with suspending, resuming, and stopping a thread

--- src/main/java/com/sayem/module1/chapter11/MyThread.java
+++ src/main/java/com/sayem/module1/chapter11/MyThread.java
@@ -1,26 +1,61 @@
 package com.sayem.module1.chapter11;
 
+// Suspending, resuming, and stopping a thread.
+
 class MyThread implements Runnable {
     Thread thrd;
-    TickTock ttOb;
+    boolean suspended;
+    boolean stopped;
 
-    // Construct a new thread.
-    MyThread(String name, TickTock tt) {
+    MyThread(String name) {
         thrd = new Thread(this, name);
-        ttOb = tt;
-        thrd.start(); // start the thread
+        suspended = false;
+        stopped = false;
+        thrd.start();
     }
 
-    // Begin execution of new thread.
+    // This is the entry point for thread.
     public void run() {
+        System.out.println(thrd.getName() + " starting.");
+        try {
+            for(int i = 1; i < 1000; i++) {
+                System.out.print(i + " ");
+                if((i%10)==0) {
+                    System.out.println();
+                    Thread.sleep(250);
+                }
 
-        if(thrd.getName().compareTo("Tick") == 0) {
-            for(int i=0; i<5; i++) ttOb.tick(true);
-            ttOb.tick(false);
+                // Use synchronized block to check suspended and stopped.
+                synchronized(this) {
+                    while(suspended) {
+                        wait();
+                    }
+                    if(stopped) break;
+                }
+            }
+        } catch (InterruptedException exc) {
+            System.out.println(thrd.getName() + " interrupted.");
         }
-        else {
-            for(int i=0; i<5; i++) ttOb.tock(true);
-            ttOb.tock(false);
-        }
+        System.out.println(thrd.getName() + " exiting.");
+    }
+
+    // Stop the thread.
+    synchronized void mystop() {
+        stopped = true;
+
+        // The following ensures that a suspended thread can be stopped.
+        suspended = false;
+        notify();
+    }
+
+    // Suspend the thread.
+    synchronized void mysuspend() {
+        suspended = true;
+    }
+
+    // Resume the thread.
+    synchronized void myresume() {
+        suspended = false;
+        notify();
     }
 }
