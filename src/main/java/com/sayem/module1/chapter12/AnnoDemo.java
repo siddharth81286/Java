6b304ccd61e9aa8cff395ba9da6a251345aa574c
Syed Sayem <syed@hotmail.com>
2013-09-20 10:15:38 -0400

    Added AnnoDemo class

---
+++ src/main/java/com/sayem/module1/chapter12/AnnoDemo.java
@@ -0,0 +1,9 @@
+package com.sayem.module1.chapter12;
+
+class AnnoDemo {
+    public static void main(String args[]) {
+        MyClass myObj = new MyClass("test");
+
+        System.out.println(myObj.getMsg());
+    }
+}
