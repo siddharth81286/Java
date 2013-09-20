e0a003b8eeb9cdf6ac3b3537b6ed67a9f08a550c
Syed Sayem <syed@hotmail.com>
2013-09-20 10:20:54 -0400

    Added A class

---
+++ src/main/java/com/sayem/module1/chapter13/A.java
@@ -0,0 +1,18 @@
+package com.sayem.module1.chapter13;
+
+class A {
+    // ...
+}
+
+class B extends A {
+    // ...
+}
+
+class C extends A {
+    // ...
+}
+
+// Note that D does NOT extend A.
+class D {
+    // ...
+}
