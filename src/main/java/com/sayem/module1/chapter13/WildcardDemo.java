e317c6786f542cfab5e0c9b0c2272ab821625eca
Syed Sayem <syed@hotmail.com>
2013-09-20 10:20:20 -0400

    Added WildcardDemo class

---
+++ src/main/java/com/sayem/module1/chapter13/WildcardDemo.java
@@ -0,0 +1,31 @@
+package com.sayem.module1.chapter13;
+
+// Demonstrate a wildcard.
+class WildcardDemo {
+    public static void main(String args[]) {
+
+        NumericFns<Integer> iOb =
+                new NumericFns<Integer>(6);
+
+        NumericFns<Double> dOb =
+                new NumericFns<Double>(-6.0);
+
+        NumericFns<Long> lOb =
+                new NumericFns<Long>(5L);
+
+        System.out.println("Testing iOb and dOb.");
+        if(iOb.absEqual(dOb))
+            System.out.println("Absolute values are equal.");
+        else
+            System.out.println("Absolute values differ.");
+
+        System.out.println();
+
+        System.out.println("Testing iOb and lOb.");
+        if(iOb.absEqual(lOb))
+            System.out.println("Absolute values are equal.");
+        else
+            System.out.println("Absolute values differ.");
+
+    }
+}
