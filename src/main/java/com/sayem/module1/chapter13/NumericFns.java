5e4ba9be485a766ef55982c129ddff688665b3ae
Syed Sayem <syed@hotmail.com>
2013-09-20 10:19:08 -0400

    Improved NumericFncs class

--- src/main/java/com/sayem/module1/chapter13/NumericFns.java
+++ src/main/java/com/sayem/module1/chapter13/NumericFns.java
@@ -1,10 +1,9 @@
 package com.sayem.module1.chapter13;
 
-// NumericFns attemps (unsuccessfully) to create
-// a generic class that can compute various
-// numeric functions, such as the reciprocal or the
-// fractional component, given any type of number.
-class NumericFns<T> {
+// In this version of NumericFns, the type argument
+// for T must be either Number, or a class derived
+// from Number.
+class NumericFns<T extends Number> {
     T num;
 
     // Pass the constructor a reference to
@@ -15,12 +14,12 @@ class NumericFns<T> {
 
     // Return the reciprocal.
     double reciprocal() {
-        return 1 / num.doubleValue(); // Error!
+        return 1 / num.doubleValue();
     }
 
     // Return the fractional component.
     double fraction() {
-        return num.doubleValue() - num.intValue(); // Error!
+        return num.doubleValue() - num.intValue();
     }
 
     // ...
