8c26bb4a23dff3f1c4497323eb5555d0539c2b6f
Syed Sayem <syed@hotmail.com>
2013-09-26 08:24:26 -0400

    Added Conversion class

--- src/main/java/com/sayem/module2/chapter3/ScopeErr.java
+++ src/main/java/com/sayem/module2/chapter3/ScopeErr.java
@@ -5,7 +5,7 @@ class ScopeErr {
     public static void main(String args[]) {
         int bar = 1;
         {              // creates a new scope
-            int bar = 2; // Compile time error -- bar already defined!
+        //    int bar = 2; // Compile time error -- bar already defined!
         }
     }
 }
