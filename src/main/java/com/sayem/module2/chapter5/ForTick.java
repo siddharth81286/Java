bd1d032fea294d2779b37aee9a0bf92bdfb64f96
Syed Sayem <syed@hotmail.com>
2013-10-13 12:44:21 -0400

    Added ForTrick v2 example

--- src/main/java/com/sayem/module2/chapter5/ForTick.java
+++ src/main/java/com/sayem/module2/chapter5/ForTick.java
@@ -9,3 +9,13 @@ class ForTick {
             System.out.println("tick " + n);
     }
 }
+
+// Declare a loop control variable inside the for.
+class ForTick2 {
+    public static void main(String args[]) {
+
+        // here, n is declared inside of the for loop
+        for(int n=10; n>0; n--)
+            System.out.println("tick " + n);
+    }
+}
