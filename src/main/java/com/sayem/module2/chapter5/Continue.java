150d2aec6a280d02a27b17bebe1facb690fd41f0
Syed Sayem <syed@hotmail.com>
2013-10-14 12:21:54 -0400

    Added Continue example

---
+++ src/main/java/com/sayem/module2/chapter5/Continue.java
@@ -0,0 +1,12 @@
+package com.sayem.module2.chapter5;
+
+// Demonstrate continue.
+class Continue {
+    public static void main(String args[]) {
+        for(int i=0; i<10; i++) {
+            System.out.print(i + " ");
+            if (i%2 == 0) continue;
+            System.out.println("");
+        }
+    }
+}
