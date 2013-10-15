a075b732ab765ab7cf0b5c8c3ac9225b007c208d
Syed Sayem <syed@hotmail.com>
2013-10-15 11:31:11 -0400

    Improved Box class

--- src/main/java/com/sayem/module2/chapter6/Box.java
+++ src/main/java/com/sayem/module2/chapter6/Box.java
@@ -4,4 +4,10 @@ class Box {
     double width;
     double height;
     double depth;
+
+    // display volume of a box
+    void volume() {
+        System.out.print("Volume is ");
+        System.out.println(width * height * depth);
+    }
 }
