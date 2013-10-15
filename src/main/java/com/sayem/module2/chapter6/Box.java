405002cbf980efd81dade2f5b7f315ba61c0aa49
Syed Sayem <syed@hotmail.com>
2013-10-15 11:34:04 -0400

    Added BoxDemo6 class

--- src/main/java/com/sayem/module2/chapter6/Box.java
+++ src/main/java/com/sayem/module2/chapter6/Box.java
@@ -1,21 +1,23 @@
 package com.sayem.module2.chapter6;
 
-// This program uses a parameterized method.
-
+/* Here, Box uses a constructor to initialize the
+   dimensions of a box.
+*/
 class Box {
     double width;
     double height;
     double depth;
 
+    // This is the constructor for Box.
+    Box() {
+        System.out.println("Constructing Box");
+        width = 10;
+        height = 10;
+        depth = 10;
+    }
+
     // compute and return volume
     double volume() {
         return width * height * depth;
-    }
-
-    // sets dimensions of box
-    void setDim(double w, double h, double d) {
-        width = w;
-        height = h;
-        depth = d;
     }
 }
