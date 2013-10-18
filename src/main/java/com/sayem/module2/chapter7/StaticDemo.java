c2c7aed7d833e76c49dc13a55ce20a7a2d35417b
Syed Sayem <syed@hotmail.com>
2013-10-18 08:23:42 -0400

    Added StaticDemo example

---
+++ src/main/java/com/sayem/module2/chapter7/StaticDemo.java
@@ -0,0 +1,9 @@
+package com.sayem.module2.chapter7;
+
+class StaticDemo {
+    static int a = 42;
+    static int b = 99;
+    static void callme() {
+        System.out.println("a = " + a);
+    }
+}
