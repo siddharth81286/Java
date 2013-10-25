9f16c5cd79db265e30aeebddccd0d592418681c2
Syed Sayem <syed@hotmail.com>
2013-10-25 08:17:04 -0400

    Improved Outer, Inner & InnerClassDemo

--- src/main/java/com/sayem/module2/chapter7/Outer.java
+++ src/main/java/com/sayem/module2/chapter7/Outer.java
@@ -1,6 +1,6 @@
 package com.sayem.module2.chapter7;
 
-// Demonstrate an inner class.
+// This program will not compile.
 class Outer {
     int outer_x = 100;
 
@@ -11,9 +11,14 @@ class Outer {
 
     // this is an innner class
     class Inner {
+        int y = 10; // y is local to Inner
         void display() {
             System.out.println("display: outer_x = " + outer_x);
         }
+    }
+
+    void showy() {
+//        System.out.println(y); // error, y not known here!
     }
 }
 
