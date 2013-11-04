0e3306f1ac58a103aa62e4a07fd2b85f126ed1b1
Syed Sayem <syed@hotmail.com>
2013-11-04 13:59:43 -0500

    Added class B

---
+++ src/main/java/com/sayem/module2/chapter8/B.java
@@ -0,0 +1,13 @@
+package com.sayem.module2.chapter8;
+
+// Create a subclass by extending class A.
+class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+    void sum() {
+        System.out.println("i+j+k: " + (i+j+k));
+    }
+}
