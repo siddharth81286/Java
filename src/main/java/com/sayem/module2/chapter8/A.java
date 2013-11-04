bef9818edd606e4b7dbc39d83930013c3aaef4fe
Syed Sayem <syed@hotmail.com>
2013-11-04 13:59:04 -0500

    Added class A

---
+++ src/main/java/com/sayem/module2/chapter8/A.java
@@ -0,0 +1,12 @@
+package com.sayem.module2.chapter8;
+
+// A simple example of inheritance.
+
+// Create a superclass.
+class A {
+    int i, j;
+
+    void showij() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
