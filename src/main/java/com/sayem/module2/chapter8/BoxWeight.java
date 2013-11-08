e86b6cb015f8bcbcef469635c7d3980348fe1f6e
Syed Sayem <syed@hotmail.com>
2013-11-08 14:00:37 -0500

    Improved Box class

--- src/main/java/com/sayem/module2/chapter8/BoxWeight.java
+++ src/main/java/com/sayem/module2/chapter8/BoxWeight.java
@@ -1,12 +1,30 @@
 package com.sayem.module2.chapter8;
 
-// BoxWeight now uses super to initialize its Box attributes.
+// BoxWeight now fully implements all constructors.
 class BoxWeight extends Box {
     double weight; // weight of box
 
-    // initialize width, height, and depth using super()
+    // construct clone of an object
+    BoxWeight(BoxWeight ob) { // pass object to constructor
+        super(ob);
+        weight = ob.weight;
+    }
+
+    // constructor when all parameters are specified
     BoxWeight(double w, double h, double d, double m) {
         super(w, h, d); // call superclass constructor
+        weight = m;
+    }
+
+    // default constructor
+    BoxWeight() {
+        super();
+        weight = -1;
+    }
+
+    // constructor used when cube is created
+    BoxWeight(double len, double m) {
+        super(len);
         weight = m;
     }
 }
