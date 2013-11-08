06e0c2c9b5cbfc342a3ae084ad060add7304223a
Syed Sayem <syed@hotmail.com>
2013-11-08 01:37:12 -0500

    Added ig03_04_05/GradeBook

---
+++ src/main/java/com/sayem/example/part1/lesson2/fig03_04_05/GradeBook.java
@@ -0,0 +1,14 @@
+package com.sayem.example.part1.lesson2.fig03_04_05;// Fig. 3.4: GradeBook.java
+// Class declaration with a method that has a parameter.
+
+public class GradeBook
+{
+   // display a welcome message to the GradeBook user
+   public void displayMessage( String courseName )
+   {
+      System.out.printf( "Welcome to the grade book for\n%s!\n", 
+         courseName );
+   } // end method displayMessage
+
+} // end class GradeBook
+
