58add03ec731be88d03adae5471cc2c9ad439d8c
Syed Sayem <syed@hotmail.com>
2013-11-08 01:35:42 -0500

    Added GradeBook

---
+++ src/main/java/com/sayem/example/part1/lesson2/fig03_01_02/GradeBook.java
@@ -0,0 +1,13 @@
+package com.sayem.example.part1.lesson2.fig03_01_02;// Fig. 3.1: GradeBook.java
+// Class declaration with one method.
+
+public class GradeBook
+{
+   // display a welcome message to the GradeBook user
+   public void displayMessage()
+   {
+      System.out.println( "Welcome to the Grade Book!" );
+   } // end method displayMessage
+
+} // end class GradeBook
+
