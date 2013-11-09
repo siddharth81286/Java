5e3afd57d3cb30be8dea2d67dfae2510fb8d8b55
Syed Sayem <syed@hotmail.com>
2013-11-09 03:39:59 -0500

    Added class A

---
+++ src/main/java/com/sayem/module2/chapter8/part4/A.java
@@ -0,0 +1,10 @@
+package com.sayem.module2.chapter8.part4;
+
+// Demonstrate when constructors are called.
+
+// Create a super class.
+class A {
+    A() {
+        System.out.println("Inside A's constructor.");
+    }
+}
