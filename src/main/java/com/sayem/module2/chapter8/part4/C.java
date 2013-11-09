a7230afa1c7e979b7ab2e5d84651de6cd847ca9f
Syed Sayem <syed@hotmail.com>
2013-11-09 03:41:03 -0500

    Added class C

---
+++ src/main/java/com/sayem/module2/chapter8/part4/C.java
@@ -0,0 +1,8 @@
+package com.sayem.module2.chapter8.part4;
+
+// Create another subclass by extending B.
+class C extends B {
+    C() {
+        System.out.println("Inside C's constructor.");
+    }
+}
