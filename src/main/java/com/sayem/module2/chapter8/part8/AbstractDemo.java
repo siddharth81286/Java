76be7b440757d95af0bc35f896298d495cdd209f
Syed Sayem <syed@hotmail.com>
2013-11-11 03:36:43 -0500

    Added AbstractDemo

---
+++ src/main/java/com/sayem/module2/chapter8/part8/AbstractDemo.java
@@ -0,0 +1,10 @@
+package com.sayem.module2.chapter8.part8;
+
+class AbstractDemo {
+    public static void main(String args[]) {
+        B b = new B();
+
+        b.callme();
+        b.callmetoo();
+    }
+}
