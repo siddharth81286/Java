963176a8cff54f575117e9e077ef9f77f1c557ff
Syed Sayem <syed@hotmail.com>
2013-11-12 16:15:33 +0600

    Added Improve balance class

--- src/main/java/com/sayem/module2/chapter9/MyPack/Balance.java
+++ src/main/java/com/sayem/module2/chapter9/MyPack/Balance.java
@@ -1,15 +1,20 @@
 package com.sayem.module2.chapter9.MyPack;
 
-class Balance {
+
+/* Now, the Balance class, its constructor, and its
+   show() method are public.  This means that they can
+   be used by non-subclass code outside their package.
+*/
+public class Balance {
     String name;
     double bal;
 
-    Balance(String n, double b) {
+    public Balance(String n, double b) {
         name = n;
         bal = b;
     }
 
-    void show() {
+    public void show() {
         if(bal<0)
             System.out.print("-->> ");
         System.out.println(name + ": $" + bal);
