bd8b9064f1ce979672bd13271e97761d99fa7af9
Syed Sayem <syed@hotmail.com>
2013-11-12 16:17:33 +0600

    Added nonIfaceMeth method to Client class

--- src/main/java/com/sayem/module2/chapter9/Client.java
+++ src/main/java/com/sayem/module2/chapter9/Client.java
@@ -5,4 +5,9 @@ class Client implements Callback {
     public void callback(int p) {
         System.out.println("callback called with " + p);
     }
+
+    void nonIfaceMeth() {
+        System.out.println("Classes that implement interfaces " +
+                "may also define other members, too.");
+    }
 }
