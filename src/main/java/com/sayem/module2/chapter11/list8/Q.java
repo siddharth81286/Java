83497a2d7b0621a078c6402f178ac329e5626d0e
Syed Sayem <syed@hotmail.com>
2013-11-17 18:35:19 +0600

    Added class Q

---
+++ src/main/java/com/sayem/module2/chapter11/list8/Q.java
@@ -0,0 +1,16 @@
+package com.sayem.module2.chapter11.list8;
+
+// An incorrect implementation of a producer and consumer.
+class Q {
+    int n;
+
+    synchronized int get() {
+        System.out.println("Got: " + n);
+        return n;
+    }
+
+    synchronized void put(int n) {
+        this.n = n;
+        System.out.println("Put: " + n);
+    }
+}
