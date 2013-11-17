b6a7973bf034840ca5baede20722b34bbe247423
Syed Sayem <syed@hotmail.com>
2013-11-17 18:33:25 +0600

    Added Callme

---
+++ src/main/java/com/sayem/module2/chapter11/list7/CallMe.java
@@ -0,0 +1,14 @@
+package com.sayem.module2.chapter11.list7;
+
+// This program uses a synchronized block.
+class Callme {
+    void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
