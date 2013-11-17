de8f1d1b1eaa98b8a32faa5a16f11c76be8e92e1
Syed Sayem <syed@hotmail.com>
2013-11-17 18:37:07 +0600

    Added Consumer

---
+++ src/main/java/com/sayem/module2/chapter11/list8/Consumer.java
@@ -0,0 +1,16 @@
+package com.sayem.module2.chapter11.list8;
+
+class Consumer implements Runnable {
+    Q q;
+
+    Consumer(Q q) {
+        this.q = q;
+        new Thread(this, "Consumer").start();
+    }
+
+    public void run() {
+        while(true) {
+            q.get();
+        }
+    }
+}
