396711c906ab7253f4ded0ff8cf137be0bb77527
Syed Sayem <syed@hotmail.com>
2013-11-18 17:28:50 +0600

    Added Apple

---
+++ src/main/java/com/sayem/module2/chapter12/list2/Apple.java
@@ -0,0 +1,8 @@
+package com.sayem.module2.chapter12.list2;
+
+// Use the built-in enumeration methods.
+
+// An enumeration of apple varieties.
+enum Apple {
+    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+}
