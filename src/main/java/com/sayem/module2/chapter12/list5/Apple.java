7d1de68eb8dac73dbff29aa50aa4296b34aea049
Syed Sayem <syed@hotmail.com>
2013-11-19 18:04:22 +0600

    Added // Demonstrate ordinal(), compareTo(), and equals().

---
+++ src/main/java/com/sayem/module2/chapter12/list5/Apple.java
@@ -0,0 +1,8 @@
+package com.sayem.module2.chapter12.list5;
+
+// Demonstrate ordinal(), compareTo(), and equals().
+
+// An enumeration of apple varieties.
+enum Apple {
+    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+}
