850b7b1988a19bd1dcf0ee7e83cd359531fba3f7
Syed Sayem <syed@hotmail.com>
2013-11-23 00:19:38 +0600

    Added SimpleApplet

---
+++ src/main/java/com/sayem/module2/Chapter13/SimpleApplet.java
@@ -0,0 +1,10 @@
+package com.sayem.module2.Chapter13;
+
+import java.awt.*;
+import java.applet.*;
+
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("A Simple Applet", 20, 20);
+    }
+}
