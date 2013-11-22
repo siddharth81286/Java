26ec9722376c97c7a99a5e04a4958706ad67ccf4
Syed Sayem <syed@hotmail.com>
2013-11-23 00:24:01 +0600

    Improved Hypot

--- src/main/java/com/sayem/module2/Chapter13/Hypot.java
+++ src/main/java/com/sayem/module2/Chapter13/Hypot.java
@@ -1,17 +1,21 @@
 package com.sayem.module2.Chapter13;
 
+// Use static import to bring sqrt() and pow() into view.
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 // Compute the hypotenuse of a right triangle.
 class Hypot {
     public static void main(String args[]) {
         double side1, side2;
         double hypot;
+
         side1 = 3.0;
         side2 = 4.0;
 
-        // Notice how sqrt() and pow() must be qualified by
-        // their class name, which is Math.
-        hypot = Math.sqrt(Math.pow(side1, 2) +
-                Math.pow(side2, 2));
+        // Here, sqrt() and pow() can be called by themselves,
+        // without their class name.
+        hypot = sqrt(pow(side1, 2) + pow(side2, 2));
 
         System.out.println("Given sides of lengths " +
                 side1 + " and " + side2 +
