72c5d7295e02d3c65428db12ed05e70ff6e55e63
Syed Sayem <syed@hotmail.com>
2013-11-23 00:22:27 +0600

    Added AssertDemo

--- src/main/java/com/sayem/module2/Chapter13/AssertDemo.java
+++ src/main/java/com/sayem/module2/Chapter13/AssertDemo.java
@@ -1,7 +1,8 @@
 package com.sayem.module2.Chapter13;
 
-// Demonstrate assert.
+// A poor way to use assert!!!
 class AssertDemo {
+    // get a random number generator
     static int val = 3;
 
     // Return an integer.
@@ -11,12 +12,11 @@ class AssertDemo {
 
     public static void main(String args[])
     {
-        int n;
+        int n = 0;
 
         for(int i=0; i < 10; i++) {
-            n = getnum();
 
-            assert n > 0; // will fail when n is 0
+            assert (n = getnum()) > 0; // This is not a good idea!
 
             System.out.println("n is " + n);
         }
