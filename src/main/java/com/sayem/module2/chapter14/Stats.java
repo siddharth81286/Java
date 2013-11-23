44537db2acb5e6cb60dba5709f1f435957843949
Syed Sayem <syed@hotmail.com>
2013-11-23 14:38:09 +0600

    Imprioved Stats<T extends Number

--- src/main/java/com/sayem/module2/chapter14/Stats.java
+++ src/main/java/com/sayem/module2/chapter14/Stats.java
@@ -1,16 +1,13 @@
 package com.sayem.module2.chapter14;
 
-// Stats attempts (unsuccessfully) to
-// create a generic class that can compute
-// the average of an array of numbers of
-// any given type.
-//
-// The class contains an error!
-class Stats<T> {
-    T[] nums; // nums is an array of type T
+// In this version of Stats, the type argument for
+// T must be either Number, or a class derived
+// from Number.
+class Stats<T extends Number> {
+    T[] nums; // array of Number or subclass
 
     // Pass the constructor a reference to
-    // an array of type T.
+    // an array of type Number or subclass.
     Stats(T[] o) {
         nums = o;
     }
@@ -20,7 +17,7 @@ class Stats<T> {
         double sum = 0.0;
 
         for(int i=0; i < nums.length; i++)
-            sum += nums[i].doubleValue(); // Error!!!
+            sum += nums[i].doubleValue();
 
         return sum / nums.length;
     }
