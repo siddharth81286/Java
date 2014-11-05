27e3811a79ae4c0f306b487f2496577fff88af8e
Syed Sayem <syed@hotmail.com>
2014-11-05 15:40:54 -0500

    Cleaning up junk

--- src/main/java/com/sayem/exercises/codingbat/warmup2/CountXX.java
+++ src/main/java/com/sayem/exercises/codingbat/warmup2/CountXX.java
@@ -1,4 +1,4 @@
-package com.sayem.exercises.codingbat.warmup1;
+package com.sayem.exercises.codingbat.warmup2;
 
 public class CountXX {
 
@@ -10,20 +10,6 @@ public class CountXX {
     }
 
     private static int countXX(String str) {
-<<<<<<< HEAD
-        char[] s = str.toCharArray();
-
         return 0;
-=======
-
-        int count = 0;
-        for (int i = 0; i < str.length() -1; i++){
-
-            if (str.substring(i, i+2).equalsIgnoreCase("xx")){
-                count++;
-            }
-        }
-        return count;
->>>>>>> aac903d884c887ba591dd0eee49c7dcef39bfd8c
     }
 }
