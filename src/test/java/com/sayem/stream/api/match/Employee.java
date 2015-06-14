03373d9548cedffed43b17841e009508657d43b7
Syed Sayem <syed@sayem.org>
2015-06-14 10:59:18 -0400

    Stream API anyMatch example

---
+++ src/test/java/com/sayem/stream/api/match/Employee.java
@@ -0,0 +1,67 @@
+package com.sayem.stream.api.match;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by syed.sayem on 6/14/15.
+ */
+public class Employee {
+
+    public static enum Gender{
+        MALE, FEMALE
+    }
+
+    private long id;
+    private String name;
+    private Gender gender;
+    private LocalDate dob;
+    private double income;
+
+    public Employee(long id, String name, Gender gender, LocalDate dob, double income) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.dob = dob;
+        this.income = income;
+    }
+
+    public boolean isMale(){
+        return this.gender == Gender.MALE;
+    }
+
+    public LocalDate getDob(){
+        return dob;
+    }
+
+    public static List<Employee> persons(){
+        Employee p1 = new Employee(1, "Jake", Gender.MALE, LocalDate.of(1971, Month.JANUARY, 1), 2342.0);
+        Employee p2 = new Employee(2, "Jack", Gender.MALE, LocalDate.of(1972,
+                Month.JULY, 21), 7100.0);
+        Employee p3 = new Employee(3, "Jane", Gender.FEMALE, LocalDate.of(1973,
+                Month.MAY, 29), 5455.0);
+        Employee p4 = new Employee(4, "Jode", Gender.MALE, LocalDate.of(1974,
+                Month.OCTOBER, 16), 1800.0);
+        Employee p5 = new Employee(5, "Jeny", Gender.FEMALE, LocalDate.of(1975,
+                Month.DECEMBER, 13), 1234.0);
+        Employee p6 = new Employee(6, "Jason", Gender.MALE, LocalDate.of(1976,
+                Month.JUNE, 9), 3211.0);
+
+        return Arrays.asList(p1, p2, p3, p4, p5, p6);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("name", name)
+                .append("gender", gender)
+                .append("dob", dob)
+                .append("income", income)
+                .toString();
+    }
+}
